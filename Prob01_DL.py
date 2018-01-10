c8d15817fd40e83a14f8a18e8ae1b7033962a07b
Kingdom <qingduo@139.com>
2018-01-10 15:03:00 +0800

    Problem 1 for Assignment 1 https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/udacity/1_notmnist.ipynb

---
+++ Prob01_DL.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+import random
+
+def show_pic(data_root):
+    os.chdir(data_root)
+    for dir_item in os.listdir(data_root):
+        if os.path.isdir(dir_item):
+            files=os.listdir(dir_item)
+            print(dir_item," has", len(files), "files.")
+            fullname=os.path.join(dir_item,files[random.randint(1,len(files))])
+            print(fullname)
+            display(Image(fullname))
+        else:
+            print(dir_item," is NOT a dir. Pass..")
+
+data_1 = '/Users/kingdom/tensorflow/udacity/notMNIST_large'
+data_2 = '/Users/kingdom/tensorflow/udacity/notMNIST_small'
+show_pic(data_1)
+show_pic(data_2)
